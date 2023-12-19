d815ffa023b31911bbba0c6ae16e4e47f58299b2
TMuln2516 <qhuynehihi@gmail.com>
2023-12-20 01:47:24 +0700

    demo20

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -2,77 +2,58 @@ package config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan("service")
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // Cho phép truy cập tất cả các tài nguyên tĩnh không bị kiểm soát bởi Spring Security
-        web.ignoring().antMatchers("/static/**", "/css/**", "/js/**", "/images/**");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new EncodingFilter(), ChannelProcessingFilter.class);
-
-        http.authorizeRequests()
-            .antMatchers("/register-user/**", "/detail/**", "/register-employer/**").permitAll()
-            .antMatchers("/user/**", "/search/**", "/job/**").hasAnyAuthority("USER", "ADMIN", "EMPLOYER")
-            .antMatchers("/admin/**").hasAuthority("ADMIN")
-            .antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER")
-            .anyRequest().permitAll()
-            .and()
-            .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/doLogin")
-                .defaultSuccessUrl("/loginSuccess")
-                .failureUrl("/login?error=true")
-            .and()
-            .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/doLogout"))
-                .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-            .and()
-            .csrf().disable(); // Tạm thời vô hiệu hóa CSRF để đơn giản hóa ví dụ, bạn nên bật nó trong môi trường thực tế.
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    @Configuration
+    public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests()
+                    .antMatchers("/register-user/**", "/detail/**", "/register-employer/**").permitAll()
+                    .antMatchers("/user/**", "/search/**", "/job/**").hasAnyAuthority("USER", "ADMIN", "EMPLOYER")
+                    .antMatchers("/admin/**").hasAuthority("ADMIN")
+                    .antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER")
+                    .anyRequest().permitAll()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/doLogin")
+                    .defaultSuccessUrl("/loginSuccess")
+                    .failureUrl("/login?error=true")
+                    .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/doLogout"))
+                    .logoutSuccessUrl("/")
+                    .deleteCookies("JSESSIONID")
+                    .and()
+                .csrf().disable(); 
+        }
     }
 }
