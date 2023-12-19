668f0eefa9e0fd0b922637ce061297d3c17ea364
TMuln2516 <qhuynehihi@gmail.com>
2023-12-20 01:16:38 +0700

    demo13

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -20,51 +20,32 @@ import service.UserDetailsServiceImpl;
 @ComponentScan("service")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Bean
-//    public UserDetailsService userDetailsService() {
-//        return new UserDetailsServiceImpl();
-//    }
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    @Override
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
 		auth.authenticationProvider(authProvider);
 	}
-    
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.addFilterBefore(new EncodingFilter(), ChannelProcessingFilter.class);
-		http
-        .authorizeRequests()
-        .antMatchers("/register-user/**").permitAll()
-        .antMatchers("/detail/**").permitAll()
-        .antMatchers("/register-employer/**").permitAll()
-        .antMatchers("/user/**", "/search/**", "/job/**").authenticated()
-        .antMatchers("/admin/**").hasAuthority("ADMIN")
-        .antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER")
-		.anyRequest().permitAll()
-		.and()
-		.formLogin()
-	      .loginPage("/login")
-	      .loginProcessingUrl("/doLogin")
-	      .defaultSuccessUrl("/loginSuccess")
-	      .failureUrl("/login?error=true")
-	    .and()
-	    .logout()
-	    .logoutRequestMatcher(new AntPathRequestMatcher("/doLogout"))
-//	      .logoutUrl("/doLogout")
-	      .logoutSuccessUrl("/")
-	      .deleteCookies("JSESSIONID")
-	      .and()
-	      .csrf();
-		}
+		http.authorizeRequests().antMatchers("/register-user/**").permitAll().antMatchers("/detail/**").permitAll()
+				.antMatchers("/register-employer/**").permitAll().antMatchers("/user/**", "/search/**", "/job/**")
+				.hasAnyAuthority("USER", "ADMIN", "EMPLOYER").antMatchers("/admin/**").hasAuthority("ADMIN")
+				.antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER").anyRequest().permitAll().and()
+				.formLogin().loginPage("/login").loginProcessingUrl("/doLogin").defaultSuccessUrl("/loginSuccess")
+				.failureUrl("/login?error=true").and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/doLogout")).logoutSuccessUrl("/")
+				.deleteCookies("JSESSIONID").and().csrf();
+	}
 }
