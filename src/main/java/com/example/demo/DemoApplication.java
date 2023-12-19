587eaec3c6fc8990ab2a293499b50311fdecbf4c
TMuln2516 <qhuynehihi@gmail.com>
2023-12-20 02:23:12 +0700

    demo26

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import config.*;
 
 @SpringBootApplication
-@ComponentScan("config.SecurityConfig")
+@ComponentScan(basePackageClasses = SecurityConfig.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
