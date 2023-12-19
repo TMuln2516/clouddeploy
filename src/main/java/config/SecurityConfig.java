4471a241623fc255c7cb2667020f2c7dbba32de0
TMuln2516 <qhuynehihi@gmail.com>
2023-12-20 01:28:37 +0700

    demo15

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -24,38 +24,41 @@ import service.UserDetailsServiceImpl;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(passwordEncoder());
-		auth.authenticationProvider(authProvider);
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilterBefore(new EncodingFilter(), ChannelProcessingFilter.class);
-		http.authorizeRequests().antMatchers("/register-user/**").permitAll().antMatchers("/detail/**").permitAll()
-				.antMatchers("/register-employer/**").permitAll().antMatchers("/user/**", "/search/**", "/job/**")
-				.hasAnyAuthority("USER", "ADMIN", "EMPLOYER").antMatchers("/admin/**").hasAuthority("ADMIN")
-				.antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER").anyRequest().permitAll().and()
-				.formLogin().loginPage("/login").loginProcessingUrl("/doLogin").defaultSuccessUrl("/loginSuccess")
-				.failureUrl("/login?error=true").and().logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/doLogout")).logoutSuccessUrl("/")
-				.deleteCookies("JSESSIONID").and().csrf();
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// Tắt cấu hình mặc định
-		web.ignoring().antMatchers("/**");
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/register-user/**").permitAll()
+            .antMatchers("/detail/**").permitAll()
+            .antMatchers("/register-employer/**").permitAll()
+            .antMatchers("/user/**", "/search/**", "/job/**").hasAnyAuthority("USER", "ADMIN", "EMPLOYER")
+            .antMatchers("/admin/**").hasAuthority("ADMIN")
+            .antMatchers("/employer/**", "/recruitment/**").hasAuthority("EMPLOYER")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/doLogin")
+            .defaultSuccessUrl("/loginSuccess")
+            .failureUrl("/login?error=true")
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/doLogout"))
+            .logoutSuccessUrl("/")
+            .deleteCookies("JSESSIONID")
+            .and()
+            .csrf();
+    }
 }
